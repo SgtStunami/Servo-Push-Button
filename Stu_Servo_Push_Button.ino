459b862279581a66fbf7ec0736998b4ebf70bdc7
SgtStunami <45580500+SgtStunami@users.noreply.github.com>
2019-03-30 17:43:00 -0400

    Add files via upload

--- Stu_Servo_Push_Button.ino
+++ Stu_Servo_Push_Button.ino
@@ -1,58 +1,44 @@
 /* Servo Learning
 by Brandon Stuhlmueller <signsbystu.com>
+This is my first program
 29 March 2019
  */
 #include <Servo.h>
-
 Servo servoA; //creates a servo object called servoa this allows you to conrol it by calling it servoa later
-int run;
-const int buttonPin = 2; //pushbutton pin
+int pos = 0; //variable to store the servo position
 const int redPin = 12; //pin the red LED is connected to
 const int greenPin = 13; //pin the yellow LED is connected to
-
-int counter = 0; //variable to keep track of button presses
-int pos = 0; //variable for position of servo
+const int bluePin = 11; //pin for blue LED 
+const int buttonPin = 2; //pushbutton pin
 int buttonState = 0; //variable for reading pushbutton status
+int randNumber; //random number
+int x = 0; //generated number holder
+int xPrevious = 0; //previous number
+int oldNumber; //old number 
 
 void setup() {
-  run = 0; //program does not move until button pressed
   servoA.attach(9); // this attaches the name servoA to pin 9 on the arduino
   pinMode(buttonPin, INPUT); // set the pushbutton input pin
   pinMode(redPin, OUTPUT); // initialize the LED pin as an output
   pinMode(greenPin, OUTPUT); // initialize the LED pin as an output
+  pinMode(bluePin, OUTPUT); // initialize the LED pin as an output
   Serial.begin(9600);//start serial port
   randomSeed(analogRead(0));  //start number generator using pin A0
 }
 
 void loop() {
-  int buttonState; // local variable to hold the pushbutton state
   buttonState = digitalRead(buttonPin); //read state of button with digitalRead and store value in buttonState
-  
-  if (buttonState == HIGH){ //Roxanne turn on the red light - keep it on until button is pressed
-  digitalWrite(redPin, HIGH); //turn on the reeeed lighhhttt
+  Serial.println(buttonState);
+  if (buttonState == HIGH){ //checks state of button
+  digitalWrite(redPin, HIGH); //Roxanne turn on the red light - keep it on until button is pressed
+  digitalWrite(greenPin, LOW); //make sure the green light is off when button is high
   } else {
-    digitalWrite(redPin, LOW); //ok you can turn off the red light
+    digitalWrite(greenPin, HIGH); //turn on the green light
+    digitalWrite(redPin, LOW); //turn off the reeeed lighhhttt
+    randNumber = random(0,180); //generate random number from 0-180
+    delay(275); //set a delay so the number has time to generate and get loaded
+    servoA.write
+    
+    
   } 
-  if (buttonState == LOW) {// turn on green led and move servoA
-  {
-    counter++;
-    delay(150);
-  } 
-  if(counter == 0)
-    servoA.write (20);  // zero degrees
-  else if(counter == 1)
-    servoA.write(180);
-  else if(counter == 2)
-    servoA.write (150); 
-  else if(counter == 3)  
-    servoA.write (180);
-  //else reset the counter to 0 which resets the servo to 0 degrees
-  else
-   counter = 0;
- // turn green LED on:
-    digitalWrite(greenPin, HIGH);
-  } else {
-    // turn LED off:
-    digitalWrite(greenPin, LOW);
-  }
 }
